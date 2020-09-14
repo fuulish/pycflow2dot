fe4c2e9620e6bc9ba409f2206230ba60f136d338
Ioannis Filippidis <jfilippidis@gmail.com>
2020-09-14 20:59:27 +0300

    REL: add Trove classifiers for Python 3.7, 3.8

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ def run_setup():
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Software Development'],
         keywords=['c', 'call graph', 'control flow', 'dot',
                   'latex', 'cflow'])
