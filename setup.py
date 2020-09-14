82f526eb03b108386f7f1ea5b29f209ed125df00
Ioannis Filippidis <jfilippidis@gmail.com>
2020-09-14 21:00:00 +0300

    REL: rm Trove classifier for Python 3.4
    Python 3.4 has reached its EOL.

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ def run_setup():
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
